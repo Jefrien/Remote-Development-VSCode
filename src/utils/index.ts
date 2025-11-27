9cb9a1928ca715d690da59739bdb1030236e3ae9
Jefrien Alvizures <jefrienalvizures@gmail.com>
2025-11-27 09:24:33 -0600

    fix bug: removes / in all paths

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,8 +16,8 @@ export function getTempDirectory() {
 export function pathServerFormat(path: string) {     
     let customPath = path.replace(/\\/g, '/');
 
-    // make path relative
-    if (customPath.startsWith('/')) {
+    // make path relative if starts with / and only one /
+    if (customPath.startsWith('/') && customPath.indexOf('/', 1) === -1) {
         customPath = customPath.substring(1);
     }
 
